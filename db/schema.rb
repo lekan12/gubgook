cf5a663f421563147253c75f1518261a0796c908
Emmanuel Faboya <efaboya@hotmail.co.uk>
2017-03-31 14:43:44 +0000

    names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319200857) do
+ActiveRecord::Schema.define(version: 20170331143956) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170319200857) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
